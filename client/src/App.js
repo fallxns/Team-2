ddc37e3ab60de23fcf527e9c2680870c63c0fa19
sugn4 <angustodd78@gmail.com>
2023-03-28 17:20:54 +0100

    add groupchat page seperation, msg box clear

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,48 @@
 import './App.css';
 import { useState } from 'react';
-import io from "socket.io-client"
-import Chat from './Chat.js';
-const socket = io.connect("http://localhost:3001")
-
+import io from 'socket.io-client';
+import Chat from './components/Chat.js';
+const socket = io.connect('http://localhost:3001');
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [group, setGroup] = useState("");
+  const [username, setUsername] = useState('');
+  const [group, setGroup] = useState('');
+  const [joined, setJoined] = useState(false);
 
-  const joinGroup = () =>{
-    if (username !== "" && group !== ""){
-      socket.emit("join_group", group)
+  const joinGroup = () => {
+    if (username !== '' && group !== '') {
+      setJoined(true);
+      socket.emit('join_group', group);
     }
-  }
+  };
 
   return (
     <div className="App">
-    <h3>Message app</h3>
-    <input
-      type = "text" 
-      placeholder="Username:" 
-      onChange={(event)=>{
-        setUsername(event.target.value);
-      }}
-    />
+      {joined === false ? (
+        <div>
+          <h3>Message app</h3>
+          <input
+            type="text"
+            placeholder="Username:"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
 
-    <input
-      type = "text" 
-      placeholder="Group name" 
-      onChange={(event)=>{
-        setGroup(event.target.value);
-      }}
-    />
-    <button onClick={joinGroup}> Join Group </button>
-
-     <Chat socket={socket} username={username} groupchat={group}/>
+          <input
+            type="text"
+            placeholder="Group name"
+            onChange={(event) => {
+              setGroup(event.target.value);
+            }}
+          />
+          <button onClick={joinGroup}> Join Group </button>
+        </div>
+      ) : (
+        <div>
+          <Chat socket={socket} username={username} groupchat={group} />
+        </div>
+      )}
     </div>
   );
 }
