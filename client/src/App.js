2dc72350bf93c2e97c956c231094076e4fcc8834
Charlie Kilner <ckilner20@gmail.com>
2023-05-10 13:48:30 +0100

    added + button for making a new message

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import { Center, ChakraProvider } from '@chakra-ui/react';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { Grid, GridItem } from '@chakra-ui/react';
 import { BeakerIcon } from '@heroicons/react/24/solid';
+import { AddIcon } from '@chakra-ui/icons';
 import {
   Avatar,
   AvatarBadge,
   AvatarGroup,
+  IconButton,
   WrapItem,
   Flex,
   Spacer,
@@ -30,7 +32,7 @@ import {
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider className="mainApp">
       <Grid
         templateAreas={`"header header"
                   "nav main"
@@ -79,12 +81,22 @@ function App() {
               <Heading size="lg" pb="20px" color="#0147FF">
                 Messages
               </Heading>
-              <Input
-                variant="outline"
-                placeholder="Search or start a new chat"
-                borderRadius="0"
-                pl="5px"
-              />
+              {/* grid rows to make a new message button */}
+              <Flex color="white">
+                <Input
+                  variant="outline"
+                  placeholder="Search or start a new chat"
+                  borderRadius="0"
+                  pl="5px"
+                />
+                <IconButton
+                  ml="6px"
+                  borderRadius="0"
+                  bg="#0147FF"
+                  aria-label="Search database"
+                  icon={<AddIcon />}
+                />
+              </Flex>
             </CardHeader>
 
             <CardBody w="100%" borderRight="1px" borderColor="#e2e8f0">
@@ -203,72 +215,27 @@ function App() {
                         </Text>
                       </Flex>
                     </GridItem>
-                    <GridItem colSpan={4}></GridItem>
-                    <GridItem rowSpan={2} colSpan={1}>
-                      <Center>
-                        <WrapItem>
-                          <Avatar
-                            name="Dan Abrahmov"
-                            src="https://bit.ly/dan-abramov"
-                          />
-                        </WrapItem>
-                      </Center>
-                    </GridItem>
-                    <GridItem colSpan={4}>
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Heading size="xs" textTransform="uppercase">
-                          Charlie kilner
-                        </Heading>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          pr="10px"
-                        >
-                          3 min ago
-                        </Text>
-                      </Flex>
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Text pt="0" fontSize="sm" fontWeight="400">
-                          Preview of the text message goes here.
-                        </Text>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          mr="10px"
-                          bg="green.200"
-                          borderColor="green.400"
-                          borderRadius="full"
-                          pr="5px"
-                          pl="5px"
-                        >
-                          <BeakerIcon className="h-6 w-6 text-blue-500" />
-                        </Text>
-                      </Flex>
-                    </GridItem>
                   </Grid>
                 </Box>
               </Stack>
             </CardBody>
           </Card>
         </GridItem>
-        <GridItem pl="2" bg="#F6F8FC" area={'main'}>
-          Message here
-        </GridItem>
         <GridItem
-          pl="2"
+          pl="0"
           bg=""
           area={'footer'}
-          pr="50px"
+          pr="20px"
           pos="bottom"
           className="footer"
+          borderTopWidth="75vh"
         >
-          <Flex minWidth="max-content" alignItems="center" gap="2">
+          <Flex
+            minWidth="max-content"
+            alignItems="center"
+            gap="2"
+            className="footer"
+          >
             <Box p="2" w="100%">
               <Input
                 variant="outline"
