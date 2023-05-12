ce0188f78d067b94a6318a21a58d3f52fd8b2493
Charlie Kilner <ckilner20@gmail.com>
2023-05-13 00:26:33 +0100

    merge with Luke plus UI changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,32 @@
-import './App.css';
-import * as React from 'react';
-import io from 'socket.io-client';
-import { useState } from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Flex } from '@chakra-ui/react';
-
 import MessageDashTab from './components/dashboardComponents/messageDashTab';
+import * as React from 'react';
+import { useState } from 'react';
+import { Center, ChakraProvider } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { BeakerIcon } from '@heroicons/react/24/solid';
+import { AddIcon } from '@chakra-ui/icons';
+import io from 'socket.io-client';
+import {
+  Avatar,
+  AvatarBadge,
+  AvatarGroup,
+  IconButton,
+  WrapItem,
+  Flex,
+  Spacer,
+  Input,
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+  Text,
+} from '@chakra-ui/react';
 
 function App() {
   const [username, setUsername] = useState('');
@@ -23,202 +44,13 @@ function App() {
 
   return (
     <ChakraProvider className="mainApp">
-      <Grid
-        templateAreas={`"header header"
-                  "nav main"
-                  "nav footer"`}
-        gridTemplateRows={'50px 50px 30px'}
-        gridTemplateColumns={'30% 70%'}
-        h="screen"
-        gap="1"
-        color="blackAlpha.700"
-        fontWeight="bold"
-      >
-        <GridItem area={'header'}>
-          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-            <GridItem w="100%" h="10" textAlign="left" pl="20px">
-              Make-It-All
-            </GridItem>
-            <GridItem w="100%" h="10">
-              <Center w="100%">
-                <Tabs>
-                  <TabList>
-                    <Tab fontSize="2xl" color="#0147FF">
-                      Messages
-                    </Tab>
-                    <Tab fontSize="2xl">Data Dashboard</Tab>
-                  </TabList>
-
-                  <TabPanels>
-                    <TabPanel>
-                      <p>Messages</p>
-                    </TabPanel>
-                    <TabPanel>
-                      <p>Data Dashboard</p>
-                    </TabPanel>
-                  </TabPanels>
-                </Tabs>
-              </Center>
-            </GridItem>
-            <GridItem w="100%" h="100%" textAlign="right" pr="30px">
-              Angus Todd
-            </GridItem>
-          </Grid>
-        </GridItem>
-        <GridItem pl="2" bg="white" area={'nav'}>
-          <Card variant="unstyled">
-            <CardHeader pb="20px" pt="0">
-              <Heading size="lg" pb="20px" color="#0147FF">
-                Messages
-              </Heading>
-              {/* grid rows to make a new message button */}
-              <Flex color="white">
-                <Input
-                  variant="outline"
-                  placeholder="Search or start a new chat"
-                  borderRadius="0"
-                  pl="5px"
-                />
-                <IconButton
-                  ml="6px"
-                  borderRadius="0"
-                  bg="#0147FF"
-                  aria-label="Search database"
-                  icon={<AddIcon />}
-                />
-              </Flex>
-            </CardHeader>
-
-            <CardBody w="100%" borderRight="1px" borderColor="#e2e8f0">
-              <Stack divider={<StackDivider />} spacing="4">
-                <Box>
-                  <Grid
-                    h="20px"
-                    pb="40px"
-                    templateRows="repeat(2, 1fr)"
-                    templateColumns="repeat(5, 1fr)"
-                    gap={4}
-                  >
-                    <GridItem rowSpan={2} colSpan={1}>
-                      <Center>
-                        <WrapItem>
-                          <Avatar
-                            name="Dan Abrahmov"
-                            src="https://bit.ly/dan-abramov"
-                          />
-                        </WrapItem>
-                      </Center>
-                    </GridItem>
-                    <GridItem colSpan={4}>
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Heading size="xs" textTransform="uppercase">
-                          Charlie kilner
-                        </Heading>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          pr="10px"
-                        >
-                          3 min ago
-                        </Text>
-                      </Flex>
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Text pt="0" fontSize="sm" fontWeight="400">
-                          Preview of the text message goes here.
-                        </Text>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          mr="10px"
-                          bg="green.200"
-                          borderColor="green.400"
-                          borderRadius="full"
-                          pr="5px"
-                          pl="5px"
-                        >
-                          <BeakerIcon className="h-6 w-6 text-blue-500" />
-                        </Text>
-                      </Flex>
-                    </GridItem>
-                    <GridItem colSpan={4}></GridItem>
-                  </Grid>
-                </Box>
-                <Box>
-                  <Grid
-                    h="20px"
-                    pb="40px"
-                    templateRows="repeat(2, 1fr)"
-                    templateColumns="repeat(5, 1fr)"
-                    gap={4}
-                  >
-                    <GridItem rowSpan={2} colSpan={1}>
-                      <Center>
-                        <WrapItem>
-                          <Avatar
-                            name="Dan Abrahmov"
-                            src="https://bit.ly/dan-abramov"
-                          />
-                        </WrapItem>
-                      </Center>
-                    </GridItem>
-                    <GridItem colSpan={4}>
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Heading size="xs" textTransform="uppercase">
-                          Charlie kilner
-                        </Heading>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          pr="10px"
-                        >
-                          3 min ago
-                        </Text>
-                      </Flex>
-
-                      <Flex minWidth="max-content" alignItems="center" gap="2">
-                        <Text pt="0" fontSize="sm" fontWeight="400">
-                          Preview of the text message goes here.
-                        </Text>
-                        <Spacer />
-                        <Text
-                          gap="2"
-                          fontSize="xs"
-                          textTransform="uppercase"
-                          fontWeight="400"
-                          mr="10px"
-                          bg="green.200"
-                          borderColor="green.400"
-                          borderRadius="full"
-                          pr="5px"
-                          pl="5px"
-                        >
-                          5+
-                        </Text>
-                      </Flex>
-                    </GridItem>
-                  </Grid>
-                </Box>
-              </Stack>
-            </CardBody>
-          </Card>
-        </GridItem>
-        <GridItem
-          pl="0"
-          bg=""
-          area={'footer'}
-          pr="20px"
-          pos="bottom"
-          className="footer"
-          borderTopWidth="75vh"
+      <div>
+        <Flex
+          paddingTop={'10px'}
+          fontSize={'xx-large'}
+          fontFamily={'heading'}
+          fontWeight={'extrabold'}
+          style={{ alignItems: 'center', justifyContent: 'center' }}
         >
           Make-It-All
         </Flex>
@@ -227,5 +59,4 @@ function App() {
     </ChakraProvider>
   );
 }
-
 export default App;
--- client/src/components/example.js
+++ client/src/components/example.js
@@ -1,51 +1,54 @@
-import { BeakerIcon } from '@heroicons/react/24/solid';
-import { AddIcon } from '@chakra-ui/icons';
-import {
-  Avatar,
-  Center,
-  Tabs,
-  TabList,
-  TabPanels,
-  Tab,
-  TabPanel,
-  IconButton,
-  WrapItem,
-  Flex,
-  Spacer,
-  Input,
-  Button,
-  ButtonGroup,
-  Card,
-  CardHeader,
-  CardBody,
-  Heading,
-  Stack,
-  StackDivider,
-  Box,
-  Text,
-  Grid,
-  GridItem,
-} from '@chakra-ui/react';
-
-function MessagingDashboard() {
+function App() {
   return (
-    <Flex>
+    <ChakraProvider className="mainApp">
       <Grid
         templateAreas={`"header header"
-              "nav main"
-              "nav footer"`}
+    "nav main"
+    "nav footer"`}
         gridTemplateRows={'50px 50px 30px'}
         gridTemplateColumns={'30% 70%'}
         h="screen"
         gap="1"
         color="blackAlpha.700"
         fontWeight="bold"
-        width="100%"
       >
+        <GridItem area={'header'}>
+          <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+            <GridItem w="100%" h="10" textAlign="left" pl="20px">
+              Make-It-All
+            </GridItem>
+            <GridItem w="100%" h="10">
+              <Center w="100%">
+                <Tabs>
+                  <TabList>
+                    <Tab fontSize="2xl" color="#0147FF">
+                      Messages
+                    </Tab>
+                    <Tab fontSize="2xl">Data Dashboard</Tab>
+                  </TabList>
+
+                  <TabPanels>
+                    <TabPanel>
+                      <p>Messages</p>
+                    </TabPanel>
+                    <TabPanel>
+                      <p>Data Dashboard</p>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </Center>
+            </GridItem>
+            <GridItem w="100%" h="100%" textAlign="right" pr="30px">
+              Angus Todd
+            </GridItem>
+          </Grid>
+        </GridItem>
         <GridItem pl="2" bg="white" area={'nav'}>
           <Card variant="unstyled">
             <CardHeader pb="20px" pt="0">
-              {/* grid rows to make a new message button */}
+              <Heading size="lg" pb="20px" color="#0147FF">
+                Messages
+              </Heading>
               <Flex color="white">
                 <Input
                   variant="outline"
@@ -123,6 +126,87 @@ function MessagingDashboard() {
                     <GridItem colSpan={4}></GridItem>
                   </Grid>
                 </Box>
+                <GridItem rowSpan={2} colSpan={1}>
+                  <Center>
+                    <Heading
+                      size="sm"
+                      textTransform="uppercase"
+                      alignItems="right"
+                      mb="8px"
+                    >
+                      team groupchat
+                    </Heading>
+                  </Center>
+                  <Center>
+                    <WrapItem gap="4px">
+                      <Avatar
+                        name="Angus Todd"
+                        src=""
+                        mb="12px"
+                        height="60px"
+                        width="60px"
+                        skewX="10px"
+                      />
+                      <Avatar
+                        name="Luke Sweeney"
+                        src=""
+                        mb="12px"
+                        height="52px"
+                        width="52px"
+                      />
+                      <Avatar
+                        name="Michae Jordan"
+                        src=""
+                        mb="12px"
+                        height="48px"
+                        width="48px"
+                      />
+                      <Avatar
+                        name="Ben J"
+                        src=""
+                        mb="12px"
+                        height="62px"
+                        width="62px"
+                      />
+                      <Avatar
+                        name="J Man"
+                        src=""
+                        mb="12px"
+                        height="48px"
+                        width="48px"
+                      />
+                      <Avatar
+                        name="Yo Yo"
+                        src=""
+                        mb="12px"
+                        height="58px"
+                        width="58px"
+                      />
+                      <Avatar
+                        name="Dan Abrahmov"
+                        src=""
+                        mb="12px"
+                        height="62px"
+                        width="62px"
+                      />
+                    </WrapItem>
+                  </Center>
+                  <Flex minWidth="max-content" alignItems="center" gap="2">
+                    <Text pt="0" fontSize="sm" fontWeight="400">
+                      Preview of the text message goes here.
+                    </Text>
+                    <Spacer />
+                    <Text
+                      gap="2"
+                      fontSize="xs"
+                      textTransform="uppercase"
+                      fontWeight="400"
+                      pr="10px"
+                    >
+                      3 min ago
+                    </Text>
+                  </Flex>
+                </GridItem>
                 <Box>
                   <Grid
                     h="20px"
@@ -163,6 +247,20 @@ function MessagingDashboard() {
                           Preview of the text message goes here.
                         </Text>
                         <Spacer />
+                        <Text
+                          gap="2"
+                          fontSize="xs"
+                          textTransform="uppercase"
+                          fontWeight="400"
+                          mr="10px"
+                          bg="green.200"
+                          borderColor="green.400"
+                          borderRadius="full"
+                          pr="5px"
+                          pl="5px"
+                        >
+                          5+
+                        </Text>
                       </Flex>
                     </GridItem>
                   </Grid>
@@ -204,8 +302,8 @@ function MessagingDashboard() {
           </Flex>
         </GridItem>
       </Grid>
-    </Flex>
+    </ChakraProvider>
   );
 }
 
-export default MessagingDashboard;
+export default App;
