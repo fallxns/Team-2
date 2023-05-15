4e0a58aacb34ab7c0b678b38bd66dd8c1bba153f
fallxns <91889980+fallxns@users.noreply.github.com>
2023-05-15 16:31:01 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,16 @@
-import './App.css';
-import { useState } from 'react';
-import io from 'socket.io-client';
-import Chat from './components/Chat.js';
-const socket = io.connect('http://localhost:3001');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  const [username, setUsername] = useState('');
-  const [group, setGroup] = useState('');
-  const [joined, setJoined] = useState(false);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  const joinGroup = () => {
-    if (username !== '' && group !== '') {
-      setJoined(true);
-      socket.emit('join_group', group);
-    }
-  };
-
-  return (
-    <div className="App">
-      {joined === false ? (
-        <div>
-          <h3>Message app</h3>
-          <input
-            type="text"
-            placeholder="Username:"
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
-
-          <input
-            type="text"
-            placeholder="Group name"
-            onChange={(event) => {
-              setGroup(event.target.value);
-            }}
-          />
-          <button onClick={joinGroup}> Join Group </button>
-        </div>
-      ) : (
-        <div>
-          <Chat socket={socket} username={username} groupchat={group} />
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
