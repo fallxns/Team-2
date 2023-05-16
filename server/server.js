08b7f1766fdb54f89c520f86a29ed1e71607b158
Mario Portillo Hernaiz <111706273+marioportillohernaiz@users.noreply.github.com>
2023-05-16 13:12:08 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -82,7 +82,7 @@ const fetchMessagesFromFirestore = async (group, callback) => {
 // Define a route for retrieving a list of line graph points
 app.get('/api/linegraph', (req, res) => {
   axios
-    .post('https://raw.githubusercontent.com/fallxns/Team-2/main/getDB/getLineGraph.php', {})
+    .post('./getDB/getLineGraph.php', {})
     .then(function (response) {
       // Handle successful graph response
       if (response.data != '') {
@@ -100,7 +100,7 @@ app.get('/api/linegraph', (req, res) => {
 // Define a route for retrieving a list of workload line graph points
 app.get('/api/workloadgraph', (req, res) => {
   axios
-    .post('https://raw.githubusercontent.com/fallxns/Team-2/main/getDB/getWorkloadGraph.php', {})
+    .post('./getDB/getWorkloadGraph.php', {})
     .then(function (response) {
       // Handle successful graph response
       if (response.data != '') {
@@ -119,7 +119,7 @@ app.get('/api/workloadgraph', (req, res) => {
 // Define a route for retrieving a list of users
 app.get('/api/login', (req, res) => {
   axios
-    .post('https://raw.githubusercontent.com/fallxns/Team-2/main/getDB/verifyLogin.php', {})
+    .post('./getDB/verifyLogin.php', {})
     .then(function (response) {
       // Handle successful response
       if (response.data != '') {
@@ -137,7 +137,7 @@ app.get('/api/login', (req, res) => {
 // Define a route for retrieving a list of teamAweekly graph points
 app.get('/api/teamAweekly', (req, res) => {
   axios
-    .post('https://raw.githubusercontent.com/fallxns/Team-2/main/getDB/getTeamAWeekly.php', {})
+    .post('./getDB/getTeamAWeekly.php', {})
     .then(function (response) {
       // Handle successful graph response
       if (response.data != '') {
@@ -155,7 +155,7 @@ app.get('/api/teamAweekly', (req, res) => {
 // Define a route for retrieving a list of team2monthly graph points
 app.get('/api/teamAmonthly', (req, res) => {
   axios
-    .post('https://raw.githubusercontent.com/fallxns/Team-2/main/getDB/getTeamAMonthly.php', {})
+    .post('./getDB/getTeamAMonthly.php', {})
     .then(function (response) {
       // Handle successful graph response
       if (response.data != '') {
