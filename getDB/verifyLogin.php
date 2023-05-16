d01c4b2eb0bbfa32fe6a35553b4cbe46d59391d7
Mario Portillo Hernaiz <111706273+marioportillohernaiz@users.noreply.github.com>
2023-05-16 13:46:43 +0100

    Update verifyLogin.php

--- getDB/verifyLogin.php
+++ getDB/verifyLogin.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST");
 header("Access-Control-Allow-Headers: Content-Type");
 
 //db credentials
-$servername = "localhost";
+$servername = "34.105.142.231";
 $username = "team2";
 $password = "Team02##";
 $database = "team2";
