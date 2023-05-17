bc869acbd64f64bde34efdc3d9cda3fea8b03ead
Mario Portillo Hernaiz <111706273+marioportillohernaiz@users.noreply.github.com>
2023-05-17 15:47:37 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -240,7 +240,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
